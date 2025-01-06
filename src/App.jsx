a1192ff7319d26daea43f44691822002842ac27c
ztionbar <ztion1990@gmail.com>
2025-01-06 22:01:45 +0800

    新增 axios 套件

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+// import axios from 'axios'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() =>{
+    (async() =>{
+      const res =await axios.get()
+      console.log(res);
+    })()
+  }, [])
 
   return (
     <>
